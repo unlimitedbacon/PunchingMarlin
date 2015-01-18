a269701633d9580d2004243aa10391946e6cc138
Chris Petersen <geek@ex-nerd.com>
2015-01-18 00:51:25 -0800

    Increase Z_PROBE_OFFSET_FROM_EXTRUDER
    Using -.18 ends up with a head crash. It's better to let users adjust their
    G29 than risk a head crash if they don't use a negative adjustment for G29.
    
    For what it's worth, the original value of this was -17.65 but my printer
    works perfectly with `G29 Z0.2` so I rounded up for a simpler number.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -521,7 +521,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
   // these are the offsets to the probe relative to the extruder tip (Hotend - Probe)
   #define X_PROBE_OFFSET_FROM_EXTRUDER -22.919
   #define Y_PROBE_OFFSET_FROM_EXTRUDER -6.304
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -18.0  // Increase this if the first layer is too thin.
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -17.6  // Increase this if the first layer is too thin (remember: it's a negative number so increase means closer to zero).
 
   #define Z_RAISE_BEFORE_HOMING 4       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
